161a83cd36aacf9fc27131b3292fadcb7d654853
jose.rubiomoral <jose.rubiomoral@avature.net>
2022-02-20 08:50:24 +0100

    style: Change indentation

--- app/src/main/java/com/zorg/wallhavenformuzei/domain/CreateArtWork.kt
+++ app/src/main/java/com/zorg/wallhavenformuzei/domain/CreateArtWork.kt
@@ -7,13 +7,13 @@ import com.zorg.wallhavenformuzei.data.model.Wallpaper
 class CreateArtWork {
     companion object {
         fun create(wallpaper: Wallpaper): Artwork = Artwork.Builder()
-                .attribution(wallpaper.attribution)
-                .byline(wallpaper.byLine)
-                .metadata(wallpaper.id)
-                .title(wallpaper.title)
-                .token(wallpaper.token)
-                .persistentUri(Uri.parse(wallpaper.persistentUri))
-                .webUri(Uri.parse(wallpaper.webUri))
-                .build()
+            .attribution(wallpaper.attribution)
+            .byline(wallpaper.byLine)
+            .metadata(wallpaper.id)
+            .title(wallpaper.title)
+            .token(wallpaper.token)
+            .persistentUri(Uri.parse(wallpaper.persistentUri))
+            .webUri(Uri.parse(wallpaper.webUri))
+            .build()
     }
 }
