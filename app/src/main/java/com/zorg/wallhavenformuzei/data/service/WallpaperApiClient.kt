1144c660b80898feef8bf4cc8916ee9c76e7bb5a
El Increible CptSpiff <elincreiblecptspiff@gmail.com>
2022-02-20 10:24:12 +0100

    style: remove blanck lines

--- app/src/main/java/com/zorg/wallhavenformuzei/data/service/WallpaperApiClient.kt
+++ app/src/main/java/com/zorg/wallhavenformuzei/data/service/WallpaperApiClient.kt
@@ -3,7 +3,6 @@ package com.zorg.wallhavenformuzei.data.service
 import android.content.Context
 import com.zorg.wallhavenformuzei.data.model.Wallpaper
 
-
 interface WallpaperApiClient {
     fun getRandomWallpaper(): Wallpaper
 }
